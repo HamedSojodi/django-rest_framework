8ba5f742eafc63cc454118311db6d17d29ec9aea
kian rayaneh <hamedsojodi1374@gmail.com>
2022-05-19 15:47:38 +0430

    permission_classes

--- A/home/views.py
+++ A/home/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render
 from rest_framework import generics
 # Create your views here.
+from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Person
 from .serializers import PersonSerializer
 
 
-# class HomeView(APIView):
-#     def get(self, request):
-#         person = Person.objects.all()
-#         ser_data = PersonSerializer(instance=person, many=True)
-#         return Response(ser_data.data)
+class HomeView(APIView):
+    permission_classes = [IsAdminUser, ]
 
-class HomeView(generics.ListAPIView):
-    queryset = Person.objects.all()
-    serializer_class = PersonSerializer
+    def get(self, request):
+        person = Person.objects.all()
+        ser_data = PersonSerializer(instance=person, many=True)
+        return Response(ser_data.data)
+
+# class HomeView(generics.ListAPIView):
+#     queryset = Person.objects.all()
+#     serializer_class = PersonSerializer
