70b0d8064213121d0ace1cf4d72ac2d2dc8642c1
kian rayaneh <hamedsojodi1374@gmail.com>
2022-05-23 16:17:21 +0430

    check_object_permissions and create custom permissions

--- A/home/views.py
+++ A/home/views.py
@@ -6,6 +6,8 @@ from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDe
 from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
+from permissions import IsOwnerOrReady
 from .models import Person, Question
 from .serializers import PersonSerializer, QuestionSerializer
 
@@ -51,42 +53,46 @@ class QuestionCreateView(APIView):
 #     serializer_class = QuestionSerializer
 
 
+class QuestionUpdateView(APIView):
+    permission_classes = [IsOwnerOrReady, ]
+
+    def put(self, request, pk):
+        question = Question.objects.get(pk=pk)
+        self.check_object_permissions(request, question)
+        ser_data = QuestionSerializer(instance=question, data=request.data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class QuestionUpdateView(UpdateAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
 #
-# class QuestionUpdateView(APIView):
-#     def put(self, request, pk):
-#         question = Question.objects.get(pk=pk)
-#         ser_data = QuestionSerializer(instance=question, data=request.data, partial=True)
-#         if ser_data.is_valid():
-#             ser_data.save()
-#             return Response(ser_data.data, status=status.HTTP_200_OK)
-#         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class QuestionUpdateView(UpdateAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
-    # lookup_field = 'pk'
-    #
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"messages": "question update successfully"})
-
-
+#     lookup_field = 'pk'
 #
-# class QuestionDeleteView(APIView):
-#     def delete(self, request, pk):
-#         question = Question.objects.get(pk=pk)
-#         question.delete()
-#         return Response({'message : question deleted'}, status=status.HTTP_200_OK)
-#
+#     def update(self, request, *args, **kwargs):
+#         instance = self.get_object()
+#         serializer = self.get_serializer(instance, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"messages": "question update successfully"})
 
 
-class QuestionDeleteView(DestroyAPIView):
-    queryset = Question.objects.all()
-    serializer_class = QuestionSerializer
+class QuestionDeleteView(APIView):
+    permission_classes = [IsOwnerOrReady, ]
 
-    def preform_destroy(self, pk):
+    def delete(self, request, pk):
         question = Question.objects.get(pk=pk)
         question.delete()
+        return Response({'message : question deleted'}, status=status.HTTP_200_OK)
+
+#
+# class QuestionDeleteView(DestroyAPIView):
+#     queryset = Question.objects.all()
+#     serializer_class = QuestionSerializer
+#
+#     def preform_destroy(self, pk):
+#         question = Question.objects.get(pk=pk)
+#         question.delete()
